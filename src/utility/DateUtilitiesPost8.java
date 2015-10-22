9be07fb5af2b7cc0450e0ecb352bdf381f3f95ac
zsummers <zsummers@L11702042765.wctc.msft>
2015-10-22 16:27:18 -0500

    Playing with DateUtilitiesPost8

--- src/utility/DateUtilitiesPost8.java
+++ src/utility/DateUtilitiesPost8.java
@@ -1,11 +1,35 @@
 package utility;
 
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+
 /**
  *
  * @author zsummers
  */
 public class DateUtilitiesPost8 {
+    /**
+     * This default version of the method will return a ZonedDateTime of now, at GMT-0
+     * @return 
+     */
+    public static ZonedDateTime getTimeInGMT(){
+        ZonedDateTime zdt;
+        zdt= ZonedDateTime.now(ZoneId.of("GMT"));
+        
+        return zdt;
+    }
+    /*
     
+    */
+    public static ZonedDateTime getTimeInGMT(int offset){
+        ZonedDateTime zdt;
+        zdt= ZonedDateTime.now(ZoneId.ofOffset("GMT", ZoneOffset.ofHours(offset)));
+        
+        return zdt;
+    }
     
-    
+    public static void main(String[] args) {
+        System.out.println(DateUtilitiesPost8.getTimeInGMT(-5));
+    }
 }
