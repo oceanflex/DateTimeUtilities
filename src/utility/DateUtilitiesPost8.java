b896aaebad437b1f1ee6792281e08ab613527971
zsummers <zsummers@L11702042765.wctc.msft>
2015-10-22 16:49:56 -0500

    Playing with DateUtilitiesPost8

--- src/utility/DateUtilitiesPost8.java
+++ src/utility/DateUtilitiesPost8.java
@@ -1,5 +1,6 @@
 package utility;
 
+import java.time.DateTimeException;
 import java.time.ZoneId;
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
@@ -22,14 +23,33 @@ public class DateUtilitiesPost8 {
     /*
     
     */
-    public static ZonedDateTime getTimeInGMT(int offset){
-        ZonedDateTime zdt;
-        zdt= ZonedDateTime.now(ZoneId.ofOffset("GMT", ZoneOffset.ofHours(offset)));
-        
+    public static ZonedDateTime getTimeInGMT(int offset)throws DateTimeException{
+        ZonedDateTime zdt = null;
+        try{
+            if(offset > -18&&offset < +18){
+                zdt= ZonedDateTime.now(ZoneOffset.ofHours(offset));
+            }else if (offset > -64800 && offset < +64800){
+                zdt= ZonedDateTime.now(ZoneOffset.ofTotalSeconds(offset));
+                throw new DateTimeException(offset+" is to large an int for hours, assuming seconds");
+            }else{
+                throw new DateTimeException(offset +" is too large an int for seconds, using default of now.");
+            }
+        }catch(DateTimeException dte){
+            throw new DateTimeException(dte.getMessage());
+        }finally{
+            if(zdt!=null){
+                return zdt;
+            }
+        }
         return zdt;
+        
     }
     
     public static void main(String[] args) {
-        System.out.println(DateUtilitiesPost8.getTimeInGMT(-5));
+        //try{
+        System.out.println(DateUtilitiesPost8.getTimeInGMT(-64999));
+//        }catch(DateTimeException dte){
+//            System.out.println("caught one");
+//        }
     }
 }
